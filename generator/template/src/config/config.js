b04d8d7bf5b1f0e972402fcd292edaf07a352580
1549469775 <1549469775@qq.com>
2019-07-05 19:17:34 +0800

    FSD

--- generator/template/src/config/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  timeout: 5000,
-  loginPath: '/login',
-  BASEURL: process.env.NODE_ENV == 'production' ? "http://gank.io/api" : "http://gank.io/api"
-}
--- generator/template/src/http/urls.js
+++
@@ -1,8 +0,0 @@
-import {
-  request,
-  request_
-}
-from "./request"
-export default {
-  gank: (data) => request('POST', '/today', data),
-}
