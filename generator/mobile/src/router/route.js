2bbce8f90dbd8fc56995ff8c6badf871342eba1b
1549469775 <1549469775@qq.com>
2019-07-05 18:07:50 +0800

    fds

--- generator/mobile/src/router/route.js
+++ generator/mobile/src/router/route.js
@@ -7,22 +7,6 @@ const github__page = [{
     auth: false
   },
   component: () => import('@views/index/index')
-}, {
-  path: "/jsonview",
-  name: 'jsonview',
-  meta: {
-    cache: true,
-    auth: false
-  },
-  component: () => import('@views/jsonview/index')
-}, {
-  path: "/mescroll",
-  name: 'mescroll',
-  meta: {
-    cache: true,
-    auth: false
-  },
-  component: () => import('@views/mescroll/index')
 }]
 
 // import llroute from "./login-route"
--- generator/mobile/src/vendor/touch.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue';
-import touch from 'vue-directive-touch';
-Vue.use(touch);
